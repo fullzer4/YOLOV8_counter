412ed83442d4de1d1b962bafccd36c2a19436e92
fullzer4 <gabrielpelizzaro@gmail.com>
2023-05-22 14:04:19 +0000

    new

--- spotify_cli/target/debug/deps/spotify_cli-065ae51deb472645.d
+++ spotify_cli/target/debug/deps/spotify_cli-065ae51deb472645.d
@@ -1,5 +1,5 @@
-/run/media/fullzer4/8b61b96a-ee7c-4a69-99e4-41120d4337b0/SpotifyCLI_CS50Final_project/spotify_cli/target/debug/deps/spotify_cli-065ae51deb472645.rmeta: src/main.rs
+/run/media/fullzer4/8b61b96a-ee7c-4a69-99e4-41120d4337b0/SpotifyCLI/spotify_cli/target/debug/deps/spotify_cli-065ae51deb472645.rmeta: src/main.rs
 
-/run/media/fullzer4/8b61b96a-ee7c-4a69-99e4-41120d4337b0/SpotifyCLI_CS50Final_project/spotify_cli/target/debug/deps/spotify_cli-065ae51deb472645.d: src/main.rs
+/run/media/fullzer4/8b61b96a-ee7c-4a69-99e4-41120d4337b0/SpotifyCLI/spotify_cli/target/debug/deps/spotify_cli-065ae51deb472645.d: src/main.rs
 
 src/main.rs:
